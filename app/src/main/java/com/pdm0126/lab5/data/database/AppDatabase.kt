94479612b4511d253fda420474debf1fc9943184
recinos <00182122@uca.edu.sv>
2026-06-10 17:59:51 -0600

    adding dao entity post appdatabase & manifest

---
+++ app/src/main/java/com/pdm0126/lab5/data/database/AppDatabase.kt
@@ -0,0 +1,29 @@
+package com.pdm0126.lab5.data.database
+
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.pdm0126.lab5.data.database.dao.PostDao
+import com.pdm0126.lab5.data.database.entities.Post
+
+@Database(entities = [Post::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun postDao(): PostDao
+}
+
+class InitDatabase : Application() {
+    companion object{
+        lateinit var database: AppDatabase
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        database = Room.databaseBuilder(
+            this,
+            AppDatabase::class.java,
+            "AppDatabase"
+        ).fallbackToDestructiveMigration(false).build()
+    }
+}
