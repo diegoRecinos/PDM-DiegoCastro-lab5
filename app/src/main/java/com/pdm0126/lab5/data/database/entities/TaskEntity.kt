ca75fb62c4d2d4a451a8543584b37588a500311a
recinos <00182122@uca.edu.sv>
2026-06-10 21:43:09 -0600

    entity and model mappers

--- app/src/main/java/com/pdm0126/lab5/data/database/entities/TaskEntity.kt
+++ app/src/main/java/com/pdm0126/lab5/data/database/entities/TaskEntity.kt
@@ -2,6 +2,7 @@ package com.pdm0126.lab5.data.database.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.pdm0126.lab5.data.model.Task
 import java.util.Date
 
 @Entity(tableName = "tasks")
@@ -10,8 +11,28 @@ data class TaskEntity(
     val id: Int = 0,
     val title: String,
     val description: String,
-    val endDate: Date = Date(),
+    val endDate: Long = Date().time,
     val isCompleted: Boolean
 )
 
+// Mapear de Entidad a Modelo
+fun TaskEntity.toModel(): Task {
+    return Task(
+        id = id,
+        title = title,
+        description = description,
+        endDate = Date(endDate),
+        isCompleted = isCompleted
+    )
+}
 
+// Mapear de Modelo a Entidad
+fun Task.toEntity(): TaskEntity {
+    return TaskEntity(
+        id = id,
+        title = title,
+        description = description,
+        endDate = endDate.time,
+        isCompleted = isCompleted
+    )
+}
---
+++ app/src/main/java/com/pdm0126/lab5/data/repositories/TaskRepository.kt
@@ -0,0 +1,7 @@
+package com.pdm0126.lab5.data.repositories
+
+import com.pdm0126.lab5.data.database.dao.TaskDao
+
+class TaskRepository(private val taskDao: TaskDao) {
+
+}
