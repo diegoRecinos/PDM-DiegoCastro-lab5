9656d64ed3ee0d34ff8809294bf06ec4ce9a41e3
recinos <00182122@uca.edu.sv>
2026-06-10 22:28:09 -0600

    fix: missing @database annotation

--- app/src/main/java/com/pdm0126/lab5/data/database/AppDatabase.kt
+++ app/src/main/java/com/pdm0126/lab5/data/database/AppDatabase.kt
@@ -1,10 +1,13 @@
 package com.pdm0126.lab5.data.database
 
 import android.app.Application
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.pdm0126.lab5.data.database.dao.TaskDao
+import com.pdm0126.lab5.data.database.entities.TaskEntity
 
+@Database(entities = [TaskEntity::class], version = 1)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun taskDao(): TaskDao
 }
