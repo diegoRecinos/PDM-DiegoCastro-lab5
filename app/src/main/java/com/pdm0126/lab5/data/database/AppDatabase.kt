b6ca921a398643327d247b573298bcc5f122ca3e
recinos <00182122@uca.edu.sv>
2026-06-12 21:06:05 -0600

    comments

--- app/src/main/java/com/pdm0126/lab5/data/database/AppDatabase.kt
+++ app/src/main/java/com/pdm0126/lab5/data/database/AppDatabase.kt
@@ -7,6 +7,7 @@ import androidx.room.RoomDatabase
 import com.pdm0126.lab5.data.database.dao.TaskDao
 import com.pdm0126.lab5.data.database.entities.TaskEntity
 
+// Definimos la base de datos sea una room database
 @Database(entities = [TaskEntity::class], version = 1)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun taskDao(): TaskDao
@@ -14,18 +15,24 @@ abstract class AppDatabase: RoomDatabase() {
 
 
 class InitDatabase: Application() {
+    //acceder a la base de datos desde cualquier lugar de la app  guarda la instancia de la base de datos
+    //se instancia un singleton
     companion object {
         lateinit var database: AppDatabase
 
     }
 
+    //antes de iniciar la app, creamos la base de datos
     override fun onCreate() {
         super.onCreate()
 
+        // inicializar la instancia y se le pasa un contexto
+        //para que se identifique la base de datos
         database = Room.databaseBuilder(
             this,
             AppDatabase::class.java,
             "app_database"
+            //evitamos que se destruya la base de datos al cambiar de versión
         ).fallbackToDestructiveMigration(false).build()
 
     }
